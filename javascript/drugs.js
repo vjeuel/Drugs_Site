6f2fb45b84c6860a4db63361b0a3437b4e9b1afe
vjeuel <vjeuel@gmail.com>
2020-05-27 08:40:14 -0500

    added new drugs and started a js file

---
+++ javascript/drugs.js
@@ -0,0 +1,58 @@
+function Drugs(name, illness, link, ) {
+   this.name = name;
+   this.illness = illness;
+   this.link = link;
+};
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","Z"]
+
+const ar = "Acid Reflux";
+const a = "Asthma";
+const b = "Bladder";
+const dor = "Diabetes (Oral)";
+const din = "Diabetes (Insulin)";
+const ad = "Anxiety / Depression";
+const hpb = "High Blood Pressure";
+const lbp = "Low Blood Pressure";
+const hc = "High Cholesterol";
+const ht = "Heart";
+const p = "Pain";
+const sa = "Sleep Apnea";
+const t = "Thyroid";
+const wp = "Water Pill";
+const pr = "Prostate";
+
+// Acid Reflux
+const Cimetidine = new Drugs("Cimetidine", ar, "https://www.drugs.com/cdi/cimetidine-tablets.html");
+const Esomeprazole = new Drugs("Esomeprazole", ar, "https://www.drugs.com/cdi/esomeprazole-injection.html");
+const Famotidine = new Drugs("Famotidine", ar, "https://www.drugs.com/famotidine.html");
+const Nexium = new Drugs("Nexium", ar, "https://www.drugs.com/nexium.html");
+const Nizatidine = new Drugs("Nizatidine", ar, "https://www.drugs.com/cdi/nizatidine-capsules-and-tablets.html");
+const Omeprazole = new Drugs("Omeprazole", ar, "https://www.drugs.com/omeprazole.html");
+const Pantoprazole = new Drugs("Pantoprazole", ar, "https://www.drugs.com/pantoprazole.html");
+const Prilosec = new Drugs("Prilosec", ar, "https://www.drugs.com/prilosec.html");
+const Ranitidine = new Drugs("Ranitidine", ar, "https://www.drugs.com/ranitidine.html");
+const Sucralfate = new Drugs("Sucralfate", ar, "https://www.drugs.com/cdi/sucralfate-oral-suspension.html");
+const Zantac = new Drugs("Zantac", ar, "https://www.drugs.com/zantac.html");
+
+// High Blood Pressure
+const Amlodipine = new Drugs("Amlodipine", hpb, "https://www.drugs.com/amlodipine.html");
+const Atenolol = new Drugs("Atenolol", hpb, "https://www.drugs.com/atenolol.html");
+const Carvedilol = new Drugs("Carvedilol", hpb, "https://www.drugs.com/carvedilol.html");
+const Clonidine = new Drugs("Clonidine", hpb, "https://www.drugs.com/clonidine.html");
+const Enalapril = new Drugs("Enalapril", hpb, "https://www.drugs.com/enalapril.html");
+const Hydralazine = new Drugs("Hydralazine", hpb, "https://www.drugs.com/mtm/hydralazine.html");
+const Hydrochlorothiazide = new Drugs("Hydrochlorothiazide", hpb, "https://www.drugs.com/hydrochlorothiazide.html");
+const Lisinopril = new Drugs("Lisinopril", hpb, "https://www.drugs.com/lisinopril.html");
+const Lopressor = new Drugs("Lopressor", hpb, "https://www.drugs.com/cdi/lopressor-hct.html");
+const Losartan = new Drugs("Losartan", hpb, "https://www.drugs.com/losartan.html");
+const Metolazone = new Drugs("Metolazone", hpb, "https://www.drugs.com/mtm/metolazone.html");
+const Metoprolol = new Drugs("Metoprolol", hpb, "https://www.drugs.com/metoprolol.html");
+const Nifedipine = new Drugs("Nifedipine", hpb, "https://www.drugs.com/nifedipine.html");
+const Olmesartan = new Drugs("Olmesartan", hpb, "https://www.drugs.com/cdi/olmesartan.html");
+const Toprol = new Drugs("Toprol", hpb, "https://www.drugs.com/toprol.html");
+const Valsartan = new Drugs("Valsartan", hpb, "https://www.drugs.com/cdi/valsartan-and-hydrochlorothiazide.html");
+
+
+
+
